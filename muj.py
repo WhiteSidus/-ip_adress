4af078bedc806a47a61c4cf31a28c28f327256a5
Whiteside <vovadxc@gmail.com>
2024-02-06 13:45:48 +0100

    Co to je

---
+++ muj.py
@@ -0,0 +1,31 @@
+print ("Vitejte v programu na prevod IP adres")
+
+spusob = input("Zadejte sbusob (Dec/Bin): ")
+
+if spusob == "Bin":
+    binary = input("Zadejte binární číslo: ")
+    #Převod na desitkové číslo
+    decimal = 0
+    for digit in binary:
+        decimal = decimal*2 + int(digit)
+        if 1 <= decimal <= 126:
+            print ("IP adress ma A class")
+        elif 128 <= decimal <= 191:
+            print ("IP adress ma B class")
+        elif 192 <= decimal <= 223:
+            print ("IP adress ma C class")
+        elif 224 <= decimal <= 239:
+            print ("IP adress ma D class")
+        elif 240 <= decimal <= 254:
+            print ("IP adress ma E class")
+        else:
+            print("Chyba")
+    print(decimal)
+
+elif spusob == "Dec":
+    decimal = int(input("Zadejte desítkové číslo: "))
+    #Převod na binární číslo
+    binary = bin(decimal)[2:]
+    print(binary)
+else:
+    print("Chyba")
---
+++ nemuj.py
@@ -0,0 +1,34 @@
+print("Vítejte v programu na převod IP adres")
+
+spusob = input("Zadejte způsob (Dec/Bin): ")
+ip_address = input("Zadejte IP adresu: ")
+
+if spusob == "Bin":
+    # Rozdělení IP adresy na jednotlivé oktety
+    octets = ip_address.split('.')
+    binary_ip = ""
+    # Převod každého oktetu na binární formát
+    for octet in octets:
+        decimal_octet = int(octet)
+        if 0 <= decimal_octet <= 255:
+            binary_octet = bin(decimal_octet)[2:].zfill(8)  # Zabezpečuje, aby každý oktet měl 8 bitů
+            binary_ip += binary_octet + '.'
+        else:
+            print("Neplatný oktet:", octet)
+            exit(1)
+    binary_ip = binary_ip[:-1]  # Odstranění poslední tečky
+    print("Binární IP adresa:", binary_ip)
+
+elif spusob == "Dec":
+    # Rozdělení binární IP adresy na jednotlivé oktety
+    binary_octets = ip_address.split('.')
+    decimal_ip = ""
+    # Převod každého binárního oktetu na desítkový formát
+    for binary_octet in binary_octets:
+        decimal_octet = str(int(binary_octet, 2))
+        decimal_ip += decimal_octet + '.'
+    decimal_ip = decimal_ip[:-1]  # Odstranění poslední tečky
+    print("Desítková IP adresa:", decimal_ip)
+
+else:
+    print("Neplatný způsob, zadejte prosím 'Dec' nebo 'Bin'.")
